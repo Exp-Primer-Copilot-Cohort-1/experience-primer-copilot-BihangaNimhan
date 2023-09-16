e864086d54d0909bb801dcdafc95fdb4ab684c74
Bihanga Nimhan <bihanganimhan@gmail.com>
2023-09-16 18:23:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skill-member.html',
+    controller: 'SkillMemberController',
+    controllerAs: 'skillMemberCtrl',
+    bindToController: true,
+    scope: {
+      member: '=',
+      skill: '='
+    }
+  };
+}
